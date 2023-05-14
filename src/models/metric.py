fe21e640a21cf3e5743f83c4f370bfd059b5af16
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-05-15 00:38:07 +0700

    hi

--- src/models/metric.py
+++ src/models/metric.py
@@ -66,9 +66,9 @@ class Metric:
     def compute(self):
         if self.metric_name == "rouge":
             result_rouge = self.metric.compute(use_stemmer=True)
-            print(type(result_rouge))
-            for k, v in result_rouge.items():
-                result_rouge[k] = round(v * 100, 4)
+            # print(type(result_rouge))
+            # for k, v in result_rouge.items():
+            #     result_rouge[k] = round(v * 100, 4)
             result = result_rouge
 
         elif self.metric_name == "bleu":
