72549d697af1183ae7853a7d1b95defd06797e9c
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-05-14 16:03:36 +0700

    hihi

--- run_training.sh
+++ run_training.sh
@@ -2,14 +2,8 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.
   --num_train_epochs 10 \
 	--output_dir "./output"  \
 	--log_file "./logs" \
-	--train_files  'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\FUSEDCHAT\\train.json' \
-	               'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\KETOD\\train.json'\
-                 'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\ORQUAC\\train.json'\
-                 'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\WOW\\train.json' \
-	--val_files 'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\FUSEDCHAT\\val.json' \
-	            'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\KETOD\\val.json'\
-              'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\ORQUAC\\val.json'\
-              'C:\ALL\OJT\gradients.baselinev1.dialogstate\data\datasets\interim\GradRes\WOW\\val.json' \
+	--train_files "/kaggle/input/data-test-gradient/train.json"\
+	--val_files "/kaggle/input/data-test-gradient/val.json"\
   --per_device_train_batch_size 16 \
   --per_device_eval_batch_size 16 \
 	--num_beams   4 \
